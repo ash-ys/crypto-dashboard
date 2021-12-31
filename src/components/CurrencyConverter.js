f834d069849fa25bce2625015bfafefe48fb52bb
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-12-31 12:04:09 +0545

    secondary

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ExchangeRate } from "./ExchangeRate"
 
 export const CurrencyConverter = () => {
+
+    const currencies = {}
     return (
         <div className='currency-converter'>
             <h2>CurrencyConverter</h2>
-
+            <div className='input-box'>
             <table>
                 <body>
                     <tr>
@@ -17,9 +19,43 @@ export const CurrencyConverter = () => {
                             value={""}
                             />
                         </td>
+                        <td>
+                            <select
+                                value = {""}
+                                name = "currency-option-1"
+                                className="currency-options"
+                                
+                            >
+                                <options></options>
+
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Secondary Currency</td>
+                        <td>
+                            <input 
+                            type="number"
+                            name = "currency-amount-2"
+                            value={""}
+                            />
+                        </td>
+                        <td>
+                            <select
+                                value = {""}
+                                name = "currency-option-2"
+                                className="currency-options"
+                                
+                            >
+                                <options></options>
+
+                            </select>
+                        </td>
                     </tr>
                     </body>
             </table>
+            </div>
+            
             <ExchangeRate/>
         </div>
     )
