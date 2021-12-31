14c88df21a529e6dc75ab1ef7fc91d6b855f2622
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-12-31 11:56:53 +0545

    input table

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -4,7 +4,22 @@ import { ExchangeRate } from "./ExchangeRate"
 export const CurrencyConverter = () => {
     return (
         <div className='currency-converter'>
-            CurrencyConverter
+            <h2>CurrencyConverter</h2>
+
+            <table>
+                <body>
+                    <tr>
+                        <td>Primary Currency</td>
+                        <td>
+                            <input 
+                            type="number"
+                            name = "currency-amount-1"
+                            value={""}
+                            />
+                        </td>
+                    </tr>
+                    </body>
+            </table>
             <ExchangeRate/>
         </div>
     )
